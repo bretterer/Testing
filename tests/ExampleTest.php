5fc5dfcdbc9a3e4416f912fba5b84e153c73b5c4
Brian Retterer <bretterer@gmail.com>
2020-10-29 14:44:26 -0400

    Test

---
+++ tests/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase {
+    /** @test */
+    public function example() {
+        $this->assertTrue(true);
+    }
+}
